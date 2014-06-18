ee64524bc25af3cb9569021c3c3fcc5494cd4906
Digital B <digital_b@xpt10168-t1.tisnt.tis.co.jp>
2014-06-18 11:02:36 +0900

    immplement perceptron network

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 type Perceptron struct {
@@ -10,23 +11,47 @@ type Perceptron struct {
 	Weight []float64
 }
 
-func (node *Perceptron) init(weightNum int) {
-	node.Weight = make([]float64, weightNum)
-	for i := 0; i < weightNum; i++ {
-		node.Weight[i] = rand.Float64()
-	}
+type NeuralNet struct {
+	MiddleNode []Perceptron
+	OutputNode []Perceptron
 }
 
-func main() {
-	node := new(Perceptron)
-	node.init(6)
-	for i := 0; i < len(node.Weight); i++ {
-		fmt.Println(node.Weight[i])
+func nodeInit(weightNum int) Perceptron {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var node Perceptron
+	node.Weight = make([]float64, weightNum)
+	for i := 0; i < weightNum; i++ {
+		node.Weight[i] = rnd.Float64()
 	}
-	// create network
+	return node
+}
 
-	// learning
+func makeNodes(weightNum int, nodeNum int) []Perceptron {
+	var nodes = make([]Perceptron, nodeNum)
+	for i := 0; i < nodeNum; i++ {
+		nodes[i] = nodeInit(weightNum)
+	}
+	return nodes
+}
 
-	// wait input
+func (net *NeuralNet) init(inputNum int, middleNum int, outputNum int) {
+	net.MiddleNode = makeNodes(inputNum, middleNum)
+	net.OutputNode = makeNodes(middleNum, outputNum)
+}
+
+/*
+func (net *NeuralNet) calc(input []int) int {
+
+}
+*/
+
+func main() {
+	var nn NeuralNet
+	nn.init(2, 3, 1)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Printf("w[%d][%d] = %f\n", i, j, nn.MiddleNode[i].Weight[j])
+		}
+	}
 
 }
